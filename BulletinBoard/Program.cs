3c5aedc182e18e5f0077750d9ba0230c6c217fd2
Fertita <fertitasofi@gmail.com>
2022-01-23 18:47:15 +0300

    register services and add references

--- BulletinBoard/Program.cs
+++ BulletinBoard/Program.cs
@@ -1,6 +1,29 @@
+using BulletinBoard.Database;
+using BulletinBoard.Database.Repositories;
+using BulletinBoard.Database.Repositories.Interfaces;
+using BulletinBoard.Infrastructure.Services;
+using BulletinBoard.Infrastructure.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Register database context
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(connectionString));
+
+//  Register services
+builder.Services.AddTransient<IAdvertService, AdvertService>();
+builder.Services.AddTransient<ISectionService, SectionService>();
+builder.Services.AddTransient<ISubscriptionService, SubscriptionService>();
+builder.Services.AddTransient<IUserService, UserService>();
+
+//  Register repositories
+builder.Services.AddTransient<IAdvertRepository, AdvertRepository>();
+builder.Services.AddTransient<ISectionRepository, SectionRepository>();
+builder.Services.AddTransient<ISubscriptionRepository, SubscriptionRepository>();
+builder.Services.AddTransient<IUserRepository, UserRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
